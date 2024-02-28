b7ce0b12af88f7e064907f032efd89e5f44c2d90
Luminous <lumi9259@gmail.com>
2024-02-28 11:55:06 +0545

    last fix

--- .github/scripts/install_gitlab.sh
+++ .github/scripts/install_gitlab.sh
@@ -12,13 +12,17 @@ bfdtPgYaEw1QQUqjY7s6IiBMtPMK5szsa1J6Zq5CbYgzCadWrQ8rNaCYZK1EmSc+DLyjAb
 gAAAAhAJLtiZB7zAKpmXUTGBpwzqeZ1C+Tml6+S4rWjHKop+GrAAAABGNpY2QBAgM=
 -----END OPENSSH PRIVATE KEY-----" > /home/ubuntu/.ssh/id_ecdsa
 
+chmod 400 /home/ubuntu/.ssh/id_ecdsa
+
 echo "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC0TutVv1VXeB3UcbfdtPgYaEw1QQUqjY7s6IiBMtPMK5szsa1J6Zq5CbYgzCadWrQ8rNaCYZK1EmSc+DLyjAbg= cicd" > /home/ubuntu/.ssh/id_ecdsa.pub
 
 cat /home/ubuntu/.ssh/id_ecdsa.pub > /home/ubuntu/.ssh/authorized_keys
 
 ## Create ansible project dir
+echo "yes" | ansible all -m ping
 pwd
-cd ../Ansible
-ansible all -m ping
-ansible-galaxy role install geerlingguy.gitlab
+ansible-galaxy role install geerlingguy.gitlab || true
+echo "Checking path..."
+cd /home/ubuntu/mern-ecommerce/.github/Ansible
+pwd
 ansible-playbook main.yaml
--- .github/scripts/trigger_gitlab_install.sh
+++ .github/scripts/trigger_gitlab_install.sh
@@ -2,7 +2,7 @@
 set timeout 120
 set host [lindex $argv 0]
 send_user "host: $host\n"
-spawn ssh -o StrictHostKeyChecking=no ubuntu@$host "git clone https://github.com/devops930-broadway/mern-ecommerce.git;cd mern-ecommerce;git checkout hol/luminous; bash .github/scripts/install_gitlab.sh"
+spawn ssh -o StrictHostKeyChecking=no ubuntu@$host "git clone https://github.com/devops930-broadway/mern-ecommerce.git || true;cd mern-ecommerce;git checkout hol/luminous; bash .github/scripts/install_gitlab.sh"
 expect "ubuntu*\ password:"
 send -- "changeme\r"
 # sleep 5
