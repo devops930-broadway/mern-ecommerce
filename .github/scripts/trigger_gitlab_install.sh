eb305b56429357e864edd25e5fc5838d0c466281
Deepak Bomjan <dpaktamang@gmail.com>
2024-02-28 11:26:58 +0545

    fix

--- .github/scripts/trigger_gitlab_install.sh
+++ .github/scripts/trigger_gitlab_install.sh
@@ -2,7 +2,7 @@
 set timeout 120
 set host [lindex $argv 0]
 send_user "host: $host\n"
-spawn ssh -o StrictHostKeyChecking=no ubuntu@$host "git clone https://github.com/devops630-broadway/mern-ecommerce.git;cd mern-ecommerce;ls;cd ~/mern-ecommerce; bash .github/scripts/install_gitlab.sh"
+spawn ssh -o StrictHostKeyChecking=no ubuntu@$host "git clone https://github.com/devops630-broadway/mern-ecommerce.git || true;cd mern-ecommerce;ls;cd ~/mern-ecommerce;git checkout production;bash .github/scripts/install_gitlab.sh"
 expect "ubuntu*\ password:"
 send -- "changeme\r"
 # sleep 5
