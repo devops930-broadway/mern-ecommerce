ab2927c630303dd1fada5d1b30fa5bb888d9d5ff
Sabim Prajapati <prajapati.sabim@gmail.com>
2024-03-01 10:14:40 +0545

    update

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/expect -f
-set timeout 120
+set timeout 180
 set host [lindex $argv 0]
-# set fe_image [fe_image $argv 1]
-# set be_image [be_image $argv 2]
-# set image_tag [image_tag $argv 3]
-# set branch_name [branch_name $argv 3]
-
 send_user "host: $host\n"
-spawn ssh -o StrictHostKeyChecking=no ubuntu@$host "git clone https://github.com/devops930-broadway/mern-ecommerce.git; cd mern-ecommerce;docker compose -f dockercompose.yml up -d"
+spawn ssh -o StrictHostKeyChecking=no ubuntu@$host "git clone https://github.com/devops930-broadway/mern-ecommerce.git; cd mern-ecommerce; docker compose -f dockercompose.yml up -d"
 expect "ubuntu*\ password:"
 send -- "changeme\r"
 # sleep 5
@@ -15,5 +10,4 @@ send -- "changeme\r"
 # sleep 10
 # send -- "exit\r"
 expect eof
-
 
